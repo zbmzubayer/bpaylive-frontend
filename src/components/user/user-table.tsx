709673b13f0a6232cae69ac6fc3a62f9b1b10b0b
ZBM Zubayer <zbmzubayer@gmail.com>
2025-03-05 00:20:58 +0600

    feat: add user table component to display and manage users

---
+++ src/components/user/user-table.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import dayjs from "dayjs";
+import { FaCalendarDays } from "react-icons/fa6";
+import { useQuery } from "@tanstack/react-query";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@heroui/table";
+import { Spinner } from "@heroui/spinner";
+import { getAllUserQuery } from "@/services/query";
+import { User } from "@/types";
+import { EditUserModal } from "@/components/user/edit-user-modal";
+import { DeleteUserModal } from "@/components/user/delete-user-modal";
+
+export function UserTable() {
+  const { data, isFetching } = useQuery(getAllUserQuery());
+  const users = data?.data as User[];
+
+  return (
+    <Table isStriped aria-label="User table" classNames={{ th: "text-sm" }}>
+      <TableHeader>
+        <TableColumn>Username</TableColumn>
+        <TableColumn>Role</TableColumn>
+        <TableColumn>
+          <div className="flex items-center gap-1">
+            <FaCalendarDays className="size-4" />
+            <span>Create Date</span>
+          </div>
+        </TableColumn>
+        <TableColumn>Action</TableColumn>
+      </TableHeader>
+      <TableBody isLoading={isFetching} loadingContent={<Spinner />} emptyContent="No data found">
+        {users?.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.role}</TableCell>
+            <TableCell>{dayjs(user.createdAt).toString()}</TableCell>
+            <TableCell className="flex items-center gap-2">
+              <EditUserModal user={user} />
+              <DeleteUserModal id={user.id} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
