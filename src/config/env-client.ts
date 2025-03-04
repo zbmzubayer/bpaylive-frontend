575d852deee03b0459307ea7a7fd7ce88cc47c41
ZBM Zubayer <zbmzubayer@gmail.com>
2025-03-05 00:25:25 +0600

    feat: add NEXT_PUBLIC_STORAGE_URL to client environment configuration

--- src/config/env-client.ts
+++ src/config/env-client.ts
@@ -2,12 +2,14 @@ import { z } from "zod";
 
 const clientEnvSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_STORAGE_URL: z.string().url(),
 });
 
 type ClientEnv = z.infer<typeof clientEnvSchema>;
 
 const clientEnvConfig: Record<keyof ClientEnv, unknown> = {
   NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  NEXT_PUBLIC_STORAGE_URL: process.env.NEXT_PUBLIC_STORAGE_URL,
 };
 
 export const ENV_CLIENT: Readonly<ClientEnv> = clientEnvSchema.parse(clientEnvConfig);
