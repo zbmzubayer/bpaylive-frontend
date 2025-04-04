8f13b30fc4911192843398464cc684c968f390b8
ZBM Zubayer <zbmzubayer@gmail.com>
2025-04-04 22:52:33 +0600

    refactor: add vertical padding to main container for improved layout spacing

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -5,7 +5,7 @@ export default function UserLayout({ children }: Readonly<{ children: React.Reac
   return (
     <div className="text-white">
       <Header />
-      <main className="container min-h-screen">{children}</main>
+      <main className="container min-h-screen py-3">{children}</main>
       <Footer />
     </div>
   );
